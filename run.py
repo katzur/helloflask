bd46689c39c5212b44404d74c5226ab59d4b5721
katzur <zurawska.kasia@gmail.com>
2022-06-06 11:01:29 +0000

    Add Bootstrap template

--- run.py
+++ run.py
@@ -1,7 +1,9 @@
 import os
-from flask import Flask,render_template
+from flask import Flask, render_template
+
 
 app = Flask(__name__)
+
 
 @app.route("/")
 def index():
@@ -15,7 +17,13 @@ def about():
 
 @app.route("/contact")
 def contact():
-    return render_template(("contact.html"))
+    return render_template("contact.html")
+
+
+@app.route("/careers")
+def careers():
+    return render_template("careers.html")
+
 
 if __name__ == "__main__":
     app.run(
